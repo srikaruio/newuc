94738b24a833d99c2d6f73badbb96ffc95f9c581
srikaruio <srikarakula123@gmail.com>
2026-04-20 00:30:39 +0530

    Coded UC4: Display multiple names

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,7 +1,14 @@
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length == 1) {
-            System.out.println("Hello, " + args[0] + "!");
+        if (args.length > 0) {
+            String names = "";
+            for (int i = 0; i < args.length; i++) {
+                names += args[i];
+                if (i < args.length - 1) {
+                    names += ", ";
+                }
+            }
+            System.out.println("Hello, " + names + "!");
         } else {
             System.out.println("Hello, World!");
         }
