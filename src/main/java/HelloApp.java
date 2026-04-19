95e2e42870f9ccffcc7c0261008899e5de251c87
srikaruio <srikarakula123@gmail.com>
2026-04-20 00:33:23 +0530

    Coded UC3: Default handling with ternary

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,10 +1,6 @@
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            String names = String.join(", ", args);
-            System.out.println("Hello, " + names + "!");
-        } else {
-            System.out.println("Hello, World!");
-        }
+        String name = (args.length > 0) ? args[0] : "World";
+        System.out.println("Hello, " + name + "!");
     }
 }
