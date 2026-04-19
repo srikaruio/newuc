1d3cb587f2c65ee49b03af80a63174c3b6a2d98c
srikaruio <srikarakula123@gmail.com>
2026-04-20 00:31:36 +0530

    Coded UC7: String join method

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,11 +1,7 @@
 public class HelloApp {
     public static void main(String[] args) {
         if (args.length > 0) {
-            StringBuilder sb = new StringBuilder();
-            for (String name : args) {
-                sb.append(name).append(", ");
-            }
-            String names = sb.substring(0, sb.length() - 2);
+            String names = String.join(", ", args);
             System.out.println("Hello, " + names + "!");
         } else {
             System.out.println("Hello, World!");
