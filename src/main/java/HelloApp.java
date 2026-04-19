d806b57753e616b0ab50af770222a33cb4e99136
srikaruio <srikarakula123@gmail.com>
2026-04-20 00:31:18 +0530

    Coded UC6: Substring method

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,14 +1,11 @@
 public class HelloApp {
     public static void main(String[] args) {
         if (args.length > 0) {
-            String names = "";
-            int count = 0;
+            StringBuilder sb = new StringBuilder();
             for (String name : args) {
-                names += name;
-                if (++count < args.length) {
-                    names += ", ";
-                }
+                sb.append(name).append(", ");
             }
+            String names = sb.substring(0, sb.length() - 2);
             System.out.println("Hello, " + names + "!");
         } else {
             System.out.println("Hello, World!");
