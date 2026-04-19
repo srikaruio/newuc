c4484f944d8a8565d9eff00e643f60d231bb1acf
srikaruio <srikarakula123@gmail.com>
2026-04-20 00:34:29 +0530

    Coded UC7: String join without loops

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -3,12 +3,8 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder sb = new StringBuilder();
-            for (String name : args) {
-                sb.append(name).append(", ");
-            }
-            String finalNames = sb.substring(0, sb.length() - 2);
-            System.out.println("Hello, " + finalNames + "!");
+            String joined = String.join(", ", args);
+            System.out.println("Hello, " + joined + "!");
         }
     }
 }
